568b45a5bf36538617bdffd2b9c9935e43836f43
Praveen S <sashapraveen@gmail.com>
2025-12-09 21:54:15 +0530

    Initial Commit

---
+++ Payments/PaymentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentsApp.swift
+//  Payments
+//
+//  Created by S, Praveen (Cognizant) on 02/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PaymentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
