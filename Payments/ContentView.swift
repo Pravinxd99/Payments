c4f19b85ce6a750bb6ab7968745239e5001377f1
Praveen S <sashapraveen@gmail.com>
2026-03-09 20:57:13 +0530

    added text

--- Payments/ContentView.swift
+++ Payments/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
                     Text(item.description)
                 }
                 List {
-                    
+                    Text("Praveen")
                 }
             }
             .padding()
